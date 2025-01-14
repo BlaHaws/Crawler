76824f0a29b5c2552d6612469031b0b9add0699e
Blaine Haws <30300255+BlaHaws@users.noreply.github.com>
2025-01-14 01:18:10 +0000

    Assistant checkpoint: Added BeautifulSoup to spider for better HTML parsing
    Assistant generated file changes:
    - spiders/pdf_spider.py: Update spider to use BeautifulSoup
    
    ---
    
    User prompt:
    
    @spiders/pdf_spider.pyneeds to use the python library BeautifulSoup and Scrapy to traverse the website, as the links are being dynamically generated at load.

--- spiders/pdf_spider.py
+++ spiders/pdf_spider.py
@@ -1,4 +1,6 @@
+
 import scrapy
+from bs4 import BeautifulSoup
 import pdfplumber
 import tabula
 import pandas as pd
@@ -11,27 +13,31 @@ class PdfSpider(scrapy.Spider):
     start_urls = ['https://www.ncdazqr.com/']
 
     def parse(self, response):
-        # Find product category links
-        for category_link in response.css(
-                'a.preFade.fadeIn::attr(href)').getall():
-            yield response.follow(category_link, callback=self.parse_category, dont_filter=True)
+        # Use BeautifulSoup to parse the response
+        soup = BeautifulSoup(response.text, 'html.parser')
+        # Find product category links with BeautifulSoup
+        category_links = soup.find_all('a', class_='preFade fadeIn')
+        for link in category_links:
+            yield response.follow(link.get('href'), callback=self.parse_category, dont_filter=True)
 
     def parse_category(self, response):
-        # Find product links and next page link
-        for product_link in response.css(
-                'a.blog-more-link.preFade.fadeIn::attr(href)').getall():
-            yield response.follow(product_link,
-                                  callback=self.parse_product)
-        next_page_link = response.css('div.older > a::attr(href)').get()
-        if next_page_link:
-            yield response.follow(next_page_link, callback=self.parse_category)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        # Find product links
+        product_links = soup.find_all('a', class_='blog-more-link preFade fadeIn')
+        for link in product_links:
+            yield response.follow(link.get('href'), callback=self.parse_product)
+        
+        # Find next page link
+        next_page = soup.select_one('div.older > a')
+        if next_page:
+            yield response.follow(next_page.get('href'), callback=self.parse_category)
 
     def parse_product(self, response):
+        soup = BeautifulSoup(response.text, 'html.parser')
         # Find Google Drive link
-        google_drive_link = response.css(
-            'p.preFade.fadeIn > a::attr(href)').get()
-        if google_drive_link:
-            yield response.follow(google_drive_link, callback=self.parse_pdf)
+        drive_link = soup.select_one('p.preFade.fadeIn > a')
+        if drive_link:
+            yield response.follow(drive_link.get('href'), callback=self.parse_pdf)
 
     def parse_pdf(self, response):
         # Download the PDF
@@ -45,27 +51,23 @@ class PdfSpider(scrapy.Spider):
                 for page in pdf.pages:
                     # Extract tables
                     tables = tabula.read_pdf(temp_file_path,
-                                             pages=page.page_number,
-                                             multiple_tables=True)
+                                           pages=page.page_number,
+                                           multiple_tables=True)
                     for table in tables:
                         # Extract table header text
                         header_text = table.df.columns.tolist()[0]
                         # Check if header contains "Cannabinoids" or "Terpenes"
                         if "Cannabinoids" in header_text:
-                            # Process table data using pandas
                             df = pd.DataFrame(table)
-                            # Append data to CSV
                             df.to_csv('extracted_data.csv',
-                                      mode='a',
-                                      header=False,
-                                      index=False)
+                                    mode='a',
+                                    header=False,
+                                    index=False)
                         elif "Terpenes" in header_text:
-                            # Process table data using pandas
                             df = pd.DataFrame(table)
-                            # Append data to CSV
                             df.to_csv('extracted_data.csv',
-                                      mode='a',
-                                      header=False,
-                                      index=False)
+                                    mode='a',
+                                    header=False,
+                                    index=False)
         # Delete the temporary file
         os.remove(temp_file_path)
